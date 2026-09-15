c3429d79f57693ea18053d710d36058966f5d70d
KAUAN ALVES PEREIRA <25203696@dsn127m1246731.cfp127aluno.local>
2026-09-15 14:11:07 -0300

    feat: adicionar box

--- app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,9 +27,19 @@ class MainActivity : ComponentActivity() {
             ComposeLayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
-                        modifier = Modifier.padding(innerPadding).background(Color.Blue)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(color = Color.Yellow)
+                            .size(
+                                width = 200.dp,
+                                height = 300.dp
+                            ),
+                        contentAlignment = Alignment.Center
                     ) {
                         Text("Aula Android")
+                        Text(
+                            text = "Com Jetpack Compose",
+                            modifier = Modifier.align(Alignment.BottomCenter))
                     }
                 }
             }
